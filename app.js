1e0a9baac8888802e24236e4ac80e29d7ad1ef3c
Ari Dizayee <Aro009h@gmail.com>
2023-01-16 07:19:47 +0100

    First commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const body = document.body;
+let lastScroll = 0;
+window.addEventListener('scroll', () => {
+    const currentScroll = window.pageYOffset
+
+    if(currentScroll <= 0) {
+        body.classList.remove("scroll-up")
+    }
+
+    if(currentScroll > lastScroll && !body.classList.contains("scroll-down")) {
+        body.classList.remove("scroll-up")
+        body.classList.add("scroll-down")
+    }
+    if(currentScroll < lastScroll && body.classList.contains("scroll-down")) {
+        body.classList.remove("scroll-down")
+        body.classList.add("scroll-up")
+    }
+
+
+
+    lastScroll = currentScroll;
+});
+
+
+document.getElementById("btn").addEventListener('click',function() {
+    document.querySelector('.container--form').style.display = 'flex';
+})
+document.getElementById("close").addEventListener('click',function() {
+    document.querySelector('.container--form').style.display = 'none';
+})
