f5ca646cc0b123eaf7c00b8cc14324ebd1219045
Ari Dizayee <Aro009h@gmail.com>
2023-01-19 06:16:26 +0100

    add mobile version

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const body = document.body;
 let lastScroll = 0;
+
 window.addEventListener('scroll', () => {
     const currentScroll = window.pageYOffset
 
@@ -15,7 +16,7 @@ window.addEventListener('scroll', () => {
         body.classList.remove("scroll-down")
         body.classList.add("scroll-up")
     }
-
+// console.log(window.pageYOffset)
 
 
     lastScroll = currentScroll;
@@ -31,4 +32,103 @@ document.getElementById("close").addEventListener('click',function() {
 
 document.getElementById('icon').addEventListener('click', function(){
     document.querySelector('ul').classList.toggle('show');
+})
+
+//computer version
+window.addEventListener('scroll', () => {
+    let scrollPos = window.scrollY;
+    let html = document.getElementById('html');
+    if(window.innerWidth > 960 && scrollPos > 200){
+        html.classList.add('active')
+        return;
+    }else{
+        html.classList.remove('active')
+        return;
+    }
+})
+window.addEventListener('scroll', () => {
+    let scrollPos = window.scrollY;
+    console.log(scrollPos)
+    let css = document.getElementById('css');
+    if(window.innerWidth > 960 && scrollPos > 200){
+        css.classList.add('active')
+        return;
+    }else{
+        css.classList.remove('active')
+        return;
+    }
+})
+window.addEventListener('scroll', () => {
+    let scrollPos = window.scrollY;
+    // // console.log(scrollPos)
+    let js = document.getElementById('js');
+    if(window.innerWidth > 960 && scrollPos > 320){
+        js.classList.add('is-active')
+        return;
+    }else{
+        js.classList.remove('is-active')
+        return;
+    }
+})
+window.addEventListener('scroll', () => {
+    let scrollPos = window.scrollY;
+    // // console.log(scrollPos)
+    let git = document.getElementById('git');
+    if(window.innerWidth > 960 && scrollPos > 200){
+        git.classList.add('active')
+        return;
+    }else{
+        git.classList.remove('active')
+        return;
+    }
+})
+
+// mobile version
+window.addEventListener('scroll', () => {
+    let scrollPos = window.scrollY;
+    // console.log(scrollPos)
+    let html = document.getElementById('html');
+    if(window.innerWidth < 960 && scrollPos > 360){
+        html.classList.add('mobile-active')
+        return;
+    }else{
+        html.classList.remove('mobile-active')
+        return;
+    }
+})
+window.addEventListener('scroll', () => {
+    let scrollPos = window.scrollY;
+    // // console.log(scrollPos)
+    let css = document.getElementById('css');
+    if(window.innerWidth < 960 && scrollPos > 380){
+        css.classList.add('mobile-active')
+        return;
+    }else{
+        css.classList.remove('mobile-active')
+        return;
+    }
+})
+window.addEventListener('scroll', () => {
+    let scrollPos = window.scrollY;
+    // // console.log(scrollPos)
+    let js = document.getElementById('js');
+    if(window.innerWidth < 960 && scrollPos > 390){
+        js.classList.add('mobile-is-active')
+        return;
+    }else{
+        js.classList.remove('mobile-is-active')
+        return;
+    }
+})
+window.addEventListener('scroll', () => {
+    let scrollPos = window.scrollY;
+    // // console.log(scrollPos)
+    let git = document.getElementById('git');
+    if(window.innerWidth < 960 && scrollPos > 400){
+        git.classList.add('mobile-active')
+        return;
+    }else{
+        git.classList.remove('mobile-active')
+        return;
+    }
 })
